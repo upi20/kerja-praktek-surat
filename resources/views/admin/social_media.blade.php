e31bad7ec343727ddb2f173b8fc987e35abb252a
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-12-02 21:19:42 +0700

    fix view crud social media

---
+++ resources/views/admin/social_media.blade.php
@@ -0,0 +1,346 @@
+@extends('templates.admin.master')
+
+@section('content')
+    <!-- Row -->
+    <div class="row row-sm">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header d-md-flex flex-row justify-content-between">
+                    <h3 class="card-title">Status Social Media</h3>
+                    <button type="button" class="btn btn-rounded btn-success" data-bs-effect="effect-scale"
+                        data-bs-toggle="modal" href="#modal-default" onclick="add()" data-target="#modal-default">
+                        <i class="bi bi-plus-lg"></i> Add
+                    </button>
+                </div>
+                <div class="card-body">
+                    <h5 class="h5">Filter Data</h5>
+                    <form action="javascript:void(0)" class="form-inline ml-md-3 mb-md-3" id="FilterForm">
+                        <div class="form-group me-md-3">
+                            <label for="filter_status">Social Media</label>
+                            <select class="form-control" id="filter_status" name="filter_status" style="max-width: 200px">
+                                <option value="">All Social Media</option>
+                                <option value="1">Dipakai</option>
+                                <option value="0">Tidak Dipakai</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-rounded btn-md btn-info" title="Refresh Filter Table">
+                            <i class="bi bi-arrow-repeat"></i> Refresh
+                        </button>
+                    </form>
+                    <div class="table-responsive table-striped">
+                        <table class="table table-bordered border-bottom" id="tbl_main">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Icon</th>
+                                    <th>Url</th>
+                                    <th>No Urut</th>
+                                    <th>Keterangan</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody> </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Row -->
+    <div class="modal fade" id="modal-default">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content modal-content-demo">
+                <div class="modal-header">
+                    <h6 class="modal-title" id="modal-default-title"></h6><button aria-label="Close"
+                        class="btn-close" data-bs-dismiss="modal"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="modal-body">
+                    <form action="javascript:void(0)" id="MainForm" name="MainForm" method="POST"
+                        enctype="multipart/form-data">
+                        <input type="hidden" name="id" id="id">
+                        <div class="form-group">
+                            <label class="form-label" for="nama">Nama <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="nama" name="nama" placeholder="Enter Nama"
+                                required="" />
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="icon">Icon <span class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="icon" name="icon"
+                                placeholder="Menggunakan fontawesome 5.5 versi gratis" required="" />
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="url">Link <span class="text-danger">*</span></label>
+                            <input type="url" class="form-control" id="url" name="url" placeholder="Link Social Media"
+                                required="" />
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="order">Nomor Urut <span
+                                    class="text-danger">*</span></label>
+                            <input type="number" class="form-control" id="order" name="order" placeholder="Urutan"
+                                required="" />
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="keterangan">Keterangan <span
+                                    class="text-danger">*</span></label>
+                            <input type="text" class="form-control" id="keterangan" name="keterangan"
+                                placeholder="Enter Keterangan" required="" />
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="status">Status</label>
+                            <select class="form-control" style="width: 100%;" required="" id="status" name="status">
+                                <option value="1">Dipakai</option>
+                                <option value="0">Tidak Dipakai</option>
+                            </select>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary" id="btn-save" form="MainForm">
+                        <li class="fa fa-save mr-1"></li> Save changes
+                    </button>
+                    <button class="btn btn-light" data-bs-dismiss="modal">
+                        <i class="bi bi-x-lg"></i>
+                        Close
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('javascript')
+    <!-- DATA TABLE JS-->
+    <script src="{{ asset('assets/templates/admin/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/templates/admin/plugins/datatable/js/dataTables.bootstrap5.js') }}"></script>
+    <script src="{{ asset('assets/templates/admin/plugins/datatable/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('assets/templates/admin/plugins/datatable/responsive.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('assets/templates/admin/plugins/datatable/responsive.bootstrap5.min.js') }}"></script>
+
+
+    {{-- sweetalert --}}
+    <script src="{{ asset('assets/templates/admin/plugins/sweet-alert/sweetalert2.all.js') }}"></script>
+
+    <script>
+        const table_html = $('#tbl_main');
+        $(document).ready(function() {
+            // datatable ====================================================================================
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            const new_table = table_html.DataTable({
+                searchDelay: 500,
+                processing: true,
+                serverSide: true,
+                // responsive: true,
+                scrollX: true,
+                aAutoWidth: false,
+                bAutoWidth: false,
+                type: 'GET',
+                ajax: {
+                    url: "{{ route('admin.social_media') }}",
+                    data: function(d) {
+                        d['filter[status]'] = $('#filter_status').val();
+                    }
+                },
+                columns: [{
+                        data: null,
+                        name: 'id',
+                        orderable: false,
+                    },
+                    {
+                        data: 'nama',
+                        name: 'nama'
+                    },
+                    {
+                        data: 'icon',
+                        name: 'icon'
+                    },
+                    {
+                        data: 'url',
+                        name: 'url'
+                    },
+                    {
+                        data: 'order',
+                        name: 'order'
+                    },
+                    {
+                        data: 'keterangan',
+                        name: 'keterangan'
+                    },
+                    {
+                        data: 'status_str',
+                        name: 'status',
+                        render(data, type, full, meta) {
+                            const class_el = full.status == 1 ? 'badge bg-success' :
+                                'badge bg-danger';
+                            return `<span class="${class_el} p-2">${full.status_str}</span>`;
+                        },
+                    },
+                    {
+                        data: 'id',
+                        name: 'id',
+                        render(data, type, full, meta) {
+                            return ` <button type="button" class="btn btn-rounded btn-primary btn-sm" title="Edit Data"
+                                data-id="${full.id}"
+                                data-nama="${full.nama}"
+                                data-url="${full.url}"
+                                data-icon="${full.icon}"
+                                data-status="${full.status}"
+                                data-keterangan="${full.keterangan}"
+                                data-order="${full.order}"
+                                onClick="editFunc(this)">
+                                <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
+                                </button>
+                                <button type="button" class="btn btn-rounded btn-danger btn-sm" title="Delete Data" onClick="deleteFunc('${data}')">
+                                <i class="fa fa-trash" aria-hidden="true"></i> Delete
+                                </button>
+                                `;
+                        },
+                        orderable: false
+                    },
+                ],
+                order: [
+                    [4, 'asc']
+                ]
+            });
+            new_table.on('draw.dt', function() {
+                var PageInfo = table_html.DataTable().page.info();
+                new_table.column(0, {
+                    page: 'current'
+                }).nodes().each(function(cell, i) {
+                    cell.innerHTML = i + 1 + PageInfo.start;
+                });
+            });
+            $('#FilterForm').submit(function(e) {
+                e.preventDefault();
+                var oTable = table_html.dataTable();
+                oTable.fnDraw(false);
+            });
+            // insertForm ===================================================================================
+            $('#MainForm').submit(function(e) {
+                e.preventDefault();
+                resetErrorAfterInput();
+                var formData = new FormData(this);
+                setBtnLoading('#btn-save', 'Save Changes');
+                const route = ($('#id').val() == '') ?
+                    "{{ route('admin.social_media.insert') }}" :
+                    "{{ route('admin.social_media.update') }}";
+                $.ajax({
+                    type: "POST",
+                    url: route,
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    data: formData,
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    success: (data) => {
+                        $("#modal-default").modal('hide');
+                        var oTable = table_html.dataTable();
+                        oTable.fnDraw(false);
+                        Swal.fire({
+                            position: 'center',
+                            icon: 'success',
+                            title: 'Data saved successfully',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                    },
+                    error: function(data) {
+                        const res = data.responseJSON ?? {};
+                        errorAfterInput = [];
+                        for (const property in res.errors) {
+                            errorAfterInput.push(property);
+                            setErrorAfterInput(res.errors[property], `#${property}`);
+                        }
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'error',
+                            title: res.message ?? 'Something went wrong',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+                    },
+                    complete: function() {
+                        setBtnLoading('#btn-save',
+                            '<li class="fa fa-save mr-1"></li> Save changes',
+                            false);
+                    }
+                });
+            });
+        });
+        function add() {
+            $('#MainForm').trigger("reset");
+            $('#modal-default-title').html("Add Social Media");
+            $('#modal-default').modal('show');
+            $('#id').val('');
+            resetErrorAfterInput();
+        }
+        function editFunc(datas) {
+            const data = datas.dataset;
+            $('#modal-default-title').html("Edit Social Media");
+            $('#modal-default').modal('show');
+            $('#MainForm').trigger("reset");
+            console.log(data);
+            $('#id').val(data.id);
+            $('#nama').val(data.nama);
+            $('#status').val(data.status);
+            $('#url').val(data.url);
+            $('#icon').val(data.icon);
+            $('#order').val(data.order);
+            $('#keterangan').val(data.keterangan);
+        }
+        function deleteFunc(id) {
+            swal.fire({
+                title: 'Are you sure?',
+                text: "Are you sure you want to proceed ?",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes'
+            }).then(function(result) {
+                if (result.value) {
+                    $.ajax({
+                        url: `{{ url('admin/social_media') }}/${id}`,
+                        type: 'DELETE',
+                        dataType: 'json',
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        beforeSend: function() {
+                            swal.fire({
+                                title: 'Please Wait..!',
+                                text: 'Is working..',
+                                onOpen: function() {
+                                    Swal.showLoading()
+                                }
+                            })
+                        },
+                        success: function(data) {
+                            Swal.fire({
+                                position: 'center',
+                                icon: 'success',
+                                title: 'Social Media  deleted successfully',
+                                showConfirmButton: false,
+                                timer: 1500
+                            })
+                            var oTable = table_html.dataTable();
+                            oTable.fnDraw(false);
+                        },
+                        complete: function() {
+                            swal.hideLoading();
+                        },
+                        error: function(jqXHR, textStatus, errorThrown) {
+                            swal.hideLoading();
+                            swal.fire("!Opps ", "Something went wrong, try again later", "error");
+                        }
+                    });
+                }
+            });
+        }
+    </script>
+@endsection
